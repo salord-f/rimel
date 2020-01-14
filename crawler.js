61dc587db62fbc0e211e323ca0dbb21596d4a2e4
Florian Salord <florian.salord@etu.univ-cotedazur.fr>
2020-01-14 08:47:45 +0100

    Fixed npm command

--- crawler.js
+++ crawler.js
@@ -1,7 +1,15 @@
-var github = require('octonode');
+const github = require('octonode');
 
-var client = github.client();
+const client = github.client();
 
 client.get('/repos/salord-f/rimel', {}, function (err, status, body, headers) {
   console.log(body); //json object
 });
+
+const ghsearch = client.search();
+
+ghsearch.repos({
+  q: 'hub+language:go',
+  sort: 'created',
+  order: 'asc'
+}, console.log); //array of search results
