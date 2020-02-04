fc0c27d3355653d36f060f117d0e64fdb625aae2
Noé Menard <noe.menard@etu.univ-cotedazur.fr>
2020-02-04 11:34:51 +0100

    Better version of the analyzer

--- analyzer.py
+++ analyzer.py
@@ -2,64 +2,104 @@ import re
 import os
 
 numberOfRepo = 0
-inDocker = 0
-inDockerCompose = 0
-inBoth = 0
-query = b'version'
+inDocker = {}
+inDockerCompose = {}
+inBoth = {}
+# query = b'version'
+queries = ['version', 'ports', 'environment', 'tests']
 repositoryClone = './repository/'
 
 
-def contains_wanted(file):
+def contains_wanted(file, word):
     print("------------------------------")
     with open(file, 'r') as fileString:
         data = (fileString.read().replace('\n', '')).encode()
-        if re.search(query, data) is not None:
+        if re.search(word.encode(), data) is not None:
             return True
 
 
-def analyze_file(file):
+def analyze_file_docker(file):
     global inDocker
+
+    for query_docker in queries:
+        if contains_wanted(file, query_docker):
+            inDocker[query_docker] = inDocker[query_docker] + 1
+
+
+def analyze_file_docker_compose(file):
     global inDockerCompose
-    global inBoth
 
-    in_docker_bool = False
-    in_docker_compose_bool = False
+    for query_compose in queries:
+        if contains_wanted(file, query_compose):
+            inDockerCompose[query_compose] = inDockerCompose[query_compose] + 1
+
+
+def isDocker(file):
     if re.search('Dockerfile*', file):
-        if contains_wanted(file):
-            inDocker += 1
-            in_docker_bool = True
-    if re.search('docker-compose*', file):
-        if contains_wanted(file):
-            inDockerCompose += 1
-            in_docker_compose_bool = True
-    if in_docker_bool and in_docker_compose_bool:
-        inBoth += 1
-        inDocker -= 1
-        inDockerCompose -= 1
-    if in_docker_bool or in_docker_compose_bool:
         return True
+    else:
+        return False
 
+
+def isDockerFile(file):
+    if re.search('docker-compose*', file):
+        return True
+    else:
+        return False
+
+
+for query in queries:
+    inDockerCompose[query] = 0
+    inDocker[query] = 0
+    inBoth[query] = 0
 
 for folder in os.listdir(repositoryClone):
+    docker = ""
+    dockerFile = ""
     numberOfRepo += 1
     print("*****************************")
     newPath = repositoryClone + folder
-    isFile = os.path.isfile(newPath)
-    isDirectory = os.path.isdir(newPath)
-    if isFile:
-        print("file : " + newPath)
-        if analyze_file(newPath):
-            break
-    elif isDirectory:
+    if os.path.isdir(newPath):
         print("directory : " + newPath)
         for folderRecu in os.listdir(newPath):
             newPathRecu = newPath + "/" + folderRecu
-            print("fileRecu : " + "/" + newPathRecu)
-            if analyze_file(newPathRecu):
-                break
-print("##############################################RESULT###########################################################")
-print("String wanted : " + str(query))
+            if os.path.isdir(newPathRecu):
+                print(newPathRecu)
+                for subFolder in os.listdir(newPathRecu):
+                    newPathRecu = newPathRecu + "/" + subFolder
+                    if os.path.isfile(newPathRecu):
+                        if isDocker(newPathRecu) and docker == "":
+                            docker = newPathRecu
+                        elif isDockerFile(newPathRecu) and dockerFile == "":
+                            dockerFile = newPathRecu
+                        print(newPathRecu)
+            elif os.path.isfile(newPathRecu):
+                if isDocker(newPathRecu) and docker == "":
+                    docker = newPathRecu
+                elif isDockerFile(newPathRecu) and dockerFile == "":
+                    dockerFile = newPathRecu
+                print(newPathRecu)
+
+        if docker != "":
+            analyze_file_docker(docker)
+        if dockerFile != "":
+            analyze_file_docker_compose(dockerFile)
+            # if isDocker(newPathRecu) and docker == "":
+            #    docker = ...
+            # elif isDockerFIle(newPathRecu) and dockerFile == "":
+            #    dockerFile = ...
+            # if analyze_file(newPathRecu):
+            # break
+
+'''for query in queries:
+    print("############################################RESULT#########################################################")
+
+    print("String wanted : " + str(query))
+    print("Number of repo analyzed : " + str(numberOfRepo))
+    print("in DockerCompose : " + str(inDockerCompose[query]))
+    print("in Docker : " + str(inDocker[query]))
+    print("in Both : " + str(inBoth[query]))
+'''
 print("Number of repo analyzed : " + str(numberOfRepo))
-print("in DockerCompose : " + str(inDockerCompose))
-print("in Docker : " + str(inDocker))
-print("in Both : " + str(inBoth))
+print("in docker file : " + str(inDocker))
+print("in docker compose : " + str(inDockerCompose))
