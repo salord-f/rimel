114d99633b91834f21562d06d6d8a02b0b7cd1ce
Baptiste Frere <baptiste.frere@etu.unice.fr>
2020-02-04 11:26:38 +0100

    Some minor adjustments and graphs are now saved as png files

--- graphs.py
+++ graphs.py
@@ -9,17 +9,21 @@ def make_autopct(values):
         return '{p:.2f}%\n{v:d}'.format(p=pct,v=val)
     return my_autopct
 
-def repartition():
+def repartitionPie(dockerfile, dockercompose, both, none):
+    """takes the number of repositories containing a dockerfile, docker-compose, both or none
+     plot the corresponding pie chart"""
     labels = ['dockerfile', 'docker-compose', 'both', 'none']
-    data = [15, 30, 40, 100]
+    data = [dockerfile, dockercompose, both, none]
     explode = (0, 0, 0, 0.1)
     fig1, ax1 = plt.subplots()
     ax1.pie(data, labels=labels, explode=explode, autopct=make_autopct(data), shadow=True, startangle=90)
     plt.title('Repartition of docker related files in the repositories')
+    plt.savefig('graphs/repartitionPie.png')
     plt.show()
 
 def keyword(data,totalRepos):
-    #data is an array of arrays
+    """takes an array of arrays[name,nbDockerfile,nbDockerCompose,nbBoth] and the total number of repositories
+     plot the corresponding histogram chart"""
     file = []
     compose = []
     both = []
@@ -43,17 +47,16 @@ def keyword(data,totalRepos):
     bins = [x + 0.5 for x in range(0, len(data)+1)]
     plt.hist([file, compose, both], weights=weights,bins=bins, color=['royalblue', 'darkblue','blueviolet'], label=['dockerfile', 'docker-compose','both'],
                 histtype='bar')  # bar est le defaut
-    plt.ylabel('files containing the word')
+    plt.ylabel('Repositories containing the keyword')
     plt.title('Keyword by type of file')
     plt.gca().yaxis.set_major_formatter(PercentFormatter(1))
     plt.xticks(legendsLoc,legends)
     plt.legend()
+    plt.savefig('graphs/keywordHist.png')
     plt.show()
 
 
-#repartition()
-
+repartitionPie(12,20,31,52)
 
 x = [ ["mongo",1,3,2],["truc",2,0,1],["Florian",3,2,0],["test",1,1,3] ]
-
 keyword(x,6)
