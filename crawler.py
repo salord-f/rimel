bf1799f39cf863939dbef984b74e67fc9bac1b35
Florian Salord <florian.salord@etu.univ-cotedazur.fr>
2020-02-04 08:06:37 +0100

    Update crawler to take regex for filenames

--- crawler.py
+++ crawler.py
@@ -12,44 +12,45 @@ garbageW10 = 0
 
 def file_is_interesting(query, file_to_analyze, repo_to_analyze):
     global garbageW10
-    if 'Dockerfile' == file_to_analyze.path or 'docker-compose.yml' == file_to_analyze.path:
+    if (re.search('Dockerfile*', file_to_analyze.path) or re.search('docker-compose*', file_to_analyze.path)) and repo_to_analyze.get_contents(file_to_analyze.path).type != "dir":
         content = base64.b64decode(file_to_analyze.content)
         if re.search(query, content) is not None:
             try:
-                wantedRepo.append(repo_to_analyze)
-                print(
-                    '====> This repo has a Dockerfile or a docker-compose.yml, and either has the keyword : ' + query.decode(
-                        'utf-8') + ', cloning.')
+                print('====> This repo has a Dockerfile or a docker-compose.yml, and has the keyword : ' + query.decode('utf-8') + ', cloning.')
                 pygit2.clone_repository(repo_to_analyze.git_url, './repository/' + repo_to_analyze.name + '/')
             except ValueError:
                 print('Repository already cloned')
             except:
                 garbageW10 += 1
-                print('cant clone because w10 is garbage')
+                print('Can\'t clone because w10 is garbage')
             finally:
                 return True
 
-def recuFolder(folders, repo_to_analyze):
+def recuFolder(folders, repo_to_analyze, recursive = False):
         while folders:
             file_content = folders.pop(0)
             if file_content.type == "dir":
-                if recuFolder(file_content.get_contents(""), repo_to_analyze):
-                    return True
+                print(file_content)
+                folder = repo_to_analyze.get_contents(file_content.path)
+                for file in folder:
+                    if file_is_interesting(query,file, repo_to_analyze):
+                        return True
+                #if recursive and recuFolder(repo_to_analyze.get_contents(file_content.path), repo_to_analyze, False):
+                #    return True
             # folders.extend(repoQuery.get_contents(file_content.path))
             else:
-                if file_is_interesting(b'mongo', file_content, repo_to_analyze):
+                if file_is_interesting(query, file_content, repo_to_analyze):
                     return True
 
 
-def contains_docker_file_or_compose_recursively(repo_to_analyze):
-    print("-------------------- Repository : " + repo_to_analyze.name+" --------------------------")
+def contains_docker_file_or_compose_recursively(repo_to_analyze, query):
     files = repo_to_analyze.get_contents("")
     folders = repo_to_analyze.get_contents("")
 
     for file in files:
-        if file_is_interesting(b'mongo',file, repo_to_analyze):
+        if file_is_interesting(query,file, repo_to_analyze):
             return True
-    recuFolder(folders, repo_to_analyze)
+    return recuFolder(folders, repo_to_analyze)
 
 
 g = Github(os.environ['TOKEN'])
@@ -58,6 +59,7 @@ repositoryClone = './repository/'
 repos = []
 wantedRepo = []
 wanted = []
+found = []
 
 x = g.get_repos()
 reposQuery = g.search_repositories("stars:>5000 topic:docker")
@@ -66,13 +68,14 @@ query = b'mongo'
 
 print('Total repos queried : ' + str(reposQuery.totalCount))
 for repoQuery in reposQuery:
-    contains_docker_file_or_compose_recursively(repoQuery)
+    print("-------------------- Repository : " + repoQuery.name+" --------------------------")
+    if contains_docker_file_or_compose_recursively(repoQuery, query):
+        found.append(repoQuery)
 
 
 print('Number of repos kept : ' + str(len(wantedRepo)))
 
-found = []
-
+'''
 count = 0
 for dir in os.listdir(repositoryClone):
     for file in os.listdir(repositoryClone + dir):
@@ -82,6 +85,8 @@ for dir in os.listdir(repositoryClone):
                 if re.search('mongo', f.read(), re.IGNORECASE):
                     count += 1
                     found.append(dir)
+
+'''
 print('number of times the string mongo has been located : ' + str(count))
 print(found)
 print(str(garbageW10))
